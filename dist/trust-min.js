089c5933fd3fc02ec576dcd5844ba823b5fe5a46
trangminie <trangminie@users.noreply.github.com>
2023-10-30 14:54:20 +0700

    call native rpc from client

--- dist/trust-min.js
+++ dist/trust-min.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae63c0d8413474380a00772ff5fb74215b38c44b32fb7c96dca693ccf447c2f4
-size 903698
+oid sha256:f7fe966daeffb17bac0412a465c6cb3ec069af65ff13b103081880a5c291489f
+size 907143
